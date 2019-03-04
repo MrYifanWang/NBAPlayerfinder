fd97ca1d0b53a8ead4e88b88354ec74657c67215
Yifan Wang <icyazufan@gmail.com>
2019-03-04 17:11:38 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+import { TopBar } from "./TopBar";
+import { Main } from "./Main";
+
+class App extends Component {
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <TopBar />
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
